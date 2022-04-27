d65975067e0f2d3212c1760d3a5cfd5dfb3394c2
Kiss, Aron <aron123dev@gmail.com>
2022-04-27 10:52:56 +0200

    Add charts example

---
+++ charts/src/app/app.component.ts
@@ -0,0 +1,75 @@
+import { Component, ViewChild } from '@angular/core';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid
+} from "ng-apexcharts";
+import { DataService } from './data.service';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+};
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: ChartOptions;
+
+  constructor(private service: DataService) {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Mért értékek",
+          data: []
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
+      },
+      title: {
+        text: "Product Trends by Month",
+        align: "left"
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"],
+          opacity: 0.5
+        }
+      },
+      xaxis: {}
+    }
+
+    setInterval(() => {
+      const datas = this.service.getDatas();
+      this.chartOptions.series = [{
+        data: datas.map(data => data.mertErtek)
+      }]
+    }, 1000);
+  }
+}
---
+++ charts/src/app/data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+interface Data {
+  sensorId: number;
+  mertErtek: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  datas: Data[] = [];
+
+  constructor() { }
+
+  randomIntFromInterval(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  getDatas(): Data[] {
+    this.datas.push({
+      sensorId: 1,
+      mertErtek: this.randomIntFromInterval(1, 100)
+    });
+    return this.datas;
+  }
+}
